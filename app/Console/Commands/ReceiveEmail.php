1226177d6ad53a1f1b63492005ea050a808580f9
Vincent Dauce <exorus.spam@gmail.com>
2017-03-23 06:37:14 +0100

    :sparkles: Added command email:receive

---
+++ app/Console/Commands/ReceiveEmail.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Email;
+use \PhpMimeMailParser\Parser;
+
+class ReceiveEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'email:receive {file? : The file of the email}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Receive email from mail server to integrate to database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info("Receiving email ...");
+
+        $parser = new Parser;
+
+        $file = $this->argument('file');
+
+        if ($file)
+        {
+            $parser->setPath($file); 
+        }
+        else
+        {
+            $parser->setStream(fopen("php://stdin", "r"));
+        }
+
+        $email = new Email;
+        $email->from = $parser->getHeader('from');
+        $email->to = $parser->getHeader('to');
+        $email->subject = $parser->getHeader('subject');
+
+        $email->save();
+    }
+}
---
+++ tests/Unit/ReceiveEmailTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Artisan;
+
+class ReceiveEmailTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_system_can_receive_email_from_file()
+    {
+    	$exitCode = Artisan::call('email:receive', ['file' => 'tests/storage/email.txt']);
+
+    	$this->assertEquals(0, $exitCode);
+    	$this->assertDatabaseHas('emails', ['subject' => 'Mail avec fichier attaché de 1ko']);
+    }
+
+    public function test_system_can_receive_email_from_stdin()
+    {
+    	//TODO with stdin
+    }
+}
