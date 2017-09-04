37e6b1f3a0f116a81661c3970eccff8efeaa8291
eXorus <exorus.spam@gmail.com>
2017-09-04 06:14:06 +0200

    🐛 Fixed link in the footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,9 +57,8 @@
       <div class="container">
         <div class="content has-text-centered">
           <p>
-            <strong>MailCare.io</strong> by <a href="http://jgthms.com">Vincent Dauce</a>. The source code is licensed
-            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-            is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC ANS 4.0</a>.
+            <strong>MailCare.io</strong> by <a href="http://vincent.dauce.fr/">Vincent Dauce</a>. The source code is licensed
+            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
           </p>
           <p>
             <a class="icon" href="https://github.com/mailcare/mailcare">
