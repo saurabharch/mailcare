086b46f3579d2984490d1bd21d3502f1820bb442
eXorus <exorus.spam@gmail.com>
2018-11-13 07:43:32 +0100

    Clean event EmailReceived

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp()
+    {
+    	parent::setUp();
+        config([
+        	'mailcare.auth' => false,
+        	'mailcare.automations' => false,
+        ]);
+    }
 }
