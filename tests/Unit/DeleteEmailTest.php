c5f342b0ff5be5e38bbed004d1411bc14a3ee6d6
mailcare <vincent@mailcare.io>
2018-12-07 12:50:11 +0000

    Update tests/Unit/DeleteEmailTest.php

--- tests/Unit/DeleteEmailTest.php
+++ tests/Unit/DeleteEmailTest.php
@@ -18,7 +18,7 @@ class DeleteEmailTest extends TestCase
         $this->artisan('mailcare:email-receive', ['file' => 'tests/storage/email_with_attachment.eml']);
 
         $this->assertCount(1, Email::all());
-        $this->assertCount(1, Email::first()->attachments());
+        $this->assertCount(1, Email::first()->attachments()->get());
 
         $email = Email::first();
         $response = $this->json('DELETE', 'api/emails/'.$email->id);
@@ -26,6 +26,6 @@ class DeleteEmailTest extends TestCase
         $response->assertStatus(200);
         $this->assertCount(0, Email::all());
         $this->assertCount(1, Email::withTrashed()->get());
-        $this->assertCount(1, Email::withTrashed()->first()->attachments());
+        $this->assertCount(1, Email::withTrashed()->first()->attachments()->get());
     }
 }
